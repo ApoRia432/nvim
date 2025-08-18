5584a82ff322c9f06c40e6a0e8b70e63e6f4bc6b
hysilens <lecterkn@proton.me>
2025-08-19 08:27:53 +0900

    update blink.cmp opts

--- lua/plugins/ui/blink.lua
+++ lua/plugins/ui/blink.lua
@@ -1,36 +1,35 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
-	version = "1.*",
-	opts = {
-		cmdline = {
-			enabled = true,
-		},
-		keymap = {
-			preset = "default",
-			["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
-			["<Tab>"] = { "select_next", "fallback_to_mappings" },
-			["<CR>"] = { "accept", "fallback" },
-		},
-		appearance = {
-			nerd_font_variant = "mono",
-		},
-		completion = {
-			ghost_text = {
-				enabled = true,
-			},
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 200,
-			},
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-			providers = {
-				lsp = { fallbacks = {} },
-			},
-		},
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
-	opts_extend = { "sources.default" },
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "1.*",
+    opts = {
+        keymap = {
+            preset = "default",
+            ["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
+            ["<Tab>"] = { "select_next", "fallback_to_mappings" },
+            ["<CR>"] = { "accept", "fallback" },
+        },
+        completion = {
+            ghost_text = {
+                enabled = false,
+            },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+            },
+        },
+        sources = {
+            default = { "lsp", "path" },
+            providers = {
+                lsp = { fallbacks = {} },
+            },
+        },
+        signature = {
+            enabled = true,
+            window = {
+                show_documentation = false,
+            }
+        }
+    },
+    opts_extend = { "sources.default" },
 }
