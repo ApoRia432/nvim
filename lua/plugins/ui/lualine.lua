fab2be756f7f5acbfcd650c40813a22c0b2fac8c
hysilens <lecterkn@proton.me>
2025-08-19 08:50:52 +0900

    update lualine opts

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,16 +1,20 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = { { "filename", path = 1 } },
-				lualine_x = { "diff", "filetype" },
-				lualine_y = { "lsp_status" },
-				lualine_z = { "location" },
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = {
+                    { "filename", path = 1 }
+                },
+                lualine_x = { "diff", "filetype" },
+                lualine_y = { "lsp_status" },
+                lualine_z = {
+                    { "diagnostics", sources = { "nvim_workspace_diagnostic" }, }
+                },
+            },
+        })
+    end,
 }
