1447352ec2a9f26a18656003cf5d6a6b34aeb907
hysilens <lecterkn@proton.me>
2025-08-19 08:03:14 +0900

    add copilot toggle keymap

--- lua/plugins/editor/copilot.lua
+++ lua/plugins/editor/copilot.lua
@@ -1,3 +1,19 @@
+local copilot_enable = true
+
+function toggleCopilot()
+    if copilot_enable then
+        vim.cmd('Copilot disable')
+        print("Copilot disable")
+    else
+        vim.cmd('Copilot enable')
+        print("Copilot enable")
+    end
+    copilot_enable = not copilot_enable
+end
+
 return {
-    "github/copilot.vim"
+    "github/copilot.vim",
+    config = function()
+        vim.keymap.set("n", "<leader>cp", toggleCopilot, { desc = "Toggle Copilot" })
+    end
 }
