2e44ff1cabff1a0069fbea358cedee5ec5a9532b
hysilens <lecterkn@proton.me>
2025-08-19 09:46:08 +0900

    add oil-lsp-diagnostics.nvim

--- lua/plugins/ui/oil.lua
+++ lua/plugins/ui/oil.lua
@@ -1,37 +1,44 @@
 function _G.get_oil_winbar()
-    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-    local dir = require("oil").get_current_dir(bufnr)
-    if dir then
-        return vim.fn.fnamemodify(dir, ":~")
-    else
-        -- If there is no current directory (e.g. over ssh), just show the buffer name
-        return vim.api.nvim_buf_get_name(0)
-    end
+	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local dir = require("oil").get_current_dir(bufnr)
+	if dir then
+		return vim.fn.fnamemodify(dir, ":~")
+	else
+		-- If there is no current directory (e.g. over ssh), just show the buffer name
+		return vim.api.nvim_buf_get_name(0)
+	end
 end
 
 return {
-    "benomahony/oil-git.nvim",
-    dependencies = {
-        {
+	{
+		"benomahony/oil-git.nvim",
+		dependencies = {
+			{
 
-            'stevearc/oil.nvim',
-            lazy = false,
-            dependencies = {
-                "echasnovski/mini.icons",
-            },
-            opts = {
-                default_file_explorer = true,
-                columns = {
-                    "icon",
-                },
-                reverse_columns = true,
-                win_options = {
-                    winbar = "%!v:lua.get_oil_winbar()",
-                },
-                view_options = {
-                    show_hidden = true,
-                },
-            },
-        }
-    }
+				"stevearc/oil.nvim",
+				lazy = false,
+				dependencies = {
+					"echasnovski/mini.icons",
+				},
+				opts = {
+					default_file_explorer = true,
+					columns = {
+						"icon",
+					},
+					reverse_columns = true,
+					win_options = {
+						winbar = "%!v:lua.get_oil_winbar()",
+					},
+					view_options = {
+						show_hidden = true,
+					},
+				},
+			},
+		},
+	},
+	{
+		"JezerM/oil-lsp-diagnostics.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		opts = {},
+	},
 }
