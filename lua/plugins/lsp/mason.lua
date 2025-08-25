2d26c82bbed3b0c1bb8c0e0c1ee8f4397370ebf0
hysilens <lecterkn@proton.me>
2025-08-26 04:46:16 +0900

    fixed lsp list

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,15 +1,18 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { "lua_ls", "gopls", "vtsls", "vue_ls" },
-    },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
-    config = function()
-        require("mason-lspconfig").setup {
-            automatic_enable = true
-        }
-    end
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason-lspconfig").setup({
+			automatic_enable = true,
+			ensure_installed = {
+				"lua_ls",
+				"gopls",
+				"vtsls",
+				"vue_ls",
+			},
+		})
+	end,
 }
