eb795111a9252d19427dd009ba62a6ac4c8c1879
ApoRia432 <lecterkn@proton.me>
2025-12-18 23:42:05 +0900

    initial commit

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,15 @@
+local vue_language_server_path = os.getenv("BUN_INSTALL") .. "/install/global/node_modules/@vue/language-server/"
+local vue_plugin = {
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+}
+
+return {
+    init_options = {
+        plugins = {
+            vue_plugin,
+        },
+    },
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+}
